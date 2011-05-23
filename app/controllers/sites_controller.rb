6d4b8c694a242606308107394d8309ea17e504af
Carlos Abad <cabad87@gmail.com>
2011-05-23 16:02:35 +0200

    Filtros de usarios para acción search

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,5 +1,5 @@
 class SitesController < ApplicationController
-  before_filter :authenticate_user!, :except => [ :index, :show ]
+  before_filter :authenticate_user!, :except => [ :index, :show, :search ]
 
   after_filter :count_visit, :only => :show
 
